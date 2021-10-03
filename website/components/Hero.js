3fb559b3be62bcb2358f5bdc5c3588d4384ed49a
Tushar Choudhari <33191895+chtushar@users.noreply.github.com>
2021-10-04 00:11:14 +0530

    :recycle: Add hero section to the website

---
+++ website/components/Hero.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <Head>
+        <title>Cric | Get live cricket score in your terminal</title>
+      </Head>
+      <h1>🏏💻 Cric</h1>
+      <h4 className='subheading'>
+        For all the cricket lovers who happens to be on the terminal all the
+        time.
+      </h4>
+    </div>
+  );
+};
+
+export default Hero;
